669e248230c8e4a3f8fc9c95f65bc2b2cc96957a
TiMESPLiNTER <github@timesplinter.ch>
2015-03-02 08:49:54 +0100

    Fixed error in StringUtils::between

--- tests/tsfw-common/StringUtilsTest.php
+++ tests/tsfw-common/StringUtilsTest.php
@@ -8,48 +8,51 @@ use timesplinter\tsfw\common\StringUtils;
  * @author Pascal Muenst <dev@timesplinter.ch>
  * @copyright Copyright (c) 2013, TiMESPLiNTER Webdevelopment
  */
-class StringUtilsTest extends \PHPUnit_Framework_TestCase {
-
-	public function testAfterFirst() {
+class StringUtilsTest extends \PHPUnit_Framework_TestCase
+{
+	public function testAfterFirst()
+	{
 		$this->assertSame(StringUtils::afterFirst('foo/bar', '/'), 'bar', 'Test 1');
 		$this->assertSame(StringUtils::afterFirst('foo/', '/'), '', 'Test 2');
 	}
 
-	public function testBeforeFirst() {
+	public function testBeforeFirst()
+	{
 		$this->assertSame(StringUtils::beforeFirst('foo/bar', '/'), 'foo', 'Test 1');
 		$this->assertSame(StringUtils::beforeFirst('/bar', '/'), '', 'Test 2');
 	}
 
-	public function testBetween() {
+	public function testBetween()
+	{
 		$this->assertSame(StringUtils::between('foobarbaz', 'foo', 'baz'), 'bar', 'Test 1');
 		$this->assertSame(StringUtils::between('foobarbaz', 'fooba', ''), 'rbaz', 'Test 2');
 		$this->assertSame(StringUtils::between('foobarbaz', 'foobar', 'baz'), '', 'Test 3');
+		$this->assertSame(StringUtils::between('foobarbaz', '', 'baz'), 'foobar', 'Test 4');
 	}
 
-	public function testInsertBeforeLast() {
+	public function testInsertBeforeLast()
+	{
 		$this->assertSame(StringUtils::insertBeforeLast('foo bar', 'bar', 'test '), 'foo test bar', 'Test 1');
 	}
 
-    public function testStartsWith() {
+    public function testStartsWith()
+    {
         $this->assertEquals(StringUtils::startsWith('foobar', 'f'), true, 'Test 1');
         $this->assertEquals(StringUtils::startsWith('foobar', 'o'), false, 'Test 2');
         $this->assertEquals(StringUtils::startsWith('foobar', 'fo'), true, 'Test 3');
         $this->assertEquals(StringUtils::startsWith('foobar', 'foobar'), true, 'Test 4');
     }
 
-    public function testEndsWith() {
+    public function testEndsWith()
+    {
         $this->assertEquals(StringUtils::endsWith('foobar', 'f'), false, 'Test 1');
         $this->assertEquals(StringUtils::endsWith('foobar', 'a'), false, 'Test 2');
         $this->assertEquals(StringUtils::endsWith('foobar', 'ar'), true, 'Test 3');
         $this->assertEquals(StringUtils::endsWith('foobar', 'foobar'), true, 'Test 4');
     }
 
-	public function testTokenize() {
-		if (defined('HHVM_VERSION')) {
-			echo 'Ignore those tests because of this issue in HHVM: https://github.com/facebook/hhvm/issues/2860';
-			return;
-		}
-
+	public function testTokenize()
+	{
 		$this->assertEquals(StringUtils::tokenize('foobarbaz', 'a'), array(
 			'foob', 'rb', 'z'
 		), 'Test 1');
